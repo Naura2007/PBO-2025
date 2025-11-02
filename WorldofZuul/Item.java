693fc933320e11a4f3dbcf803a26ead04ed9d771
Naura Rossa Azalia <nauraazalia2007@gmail.com>
2025-11-02 22:16:27 +0700

    Add Item class with name, description, and usability

---
+++ WorldofZuul/Item.java
@@ -0,0 +1,28 @@
+public class Item {
+    private String name;
+    private String description;
+    private boolean usable;
+
+    public Item(String name, String description, boolean usable) {
+        this.name = name;
+        this.description = description;
+        this.usable = usable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isUsable() {
+        return usable;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + description;
+    }
+}
