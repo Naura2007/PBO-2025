028ae1b22df7b990d9e42026037f76fdbc498929
Naura Rossa Azalia <nauraazalia2007@gmail.com>
2025-11-09 22:10:42 +0700

    Create Mobil.java

---
+++ RentalApp/Mobil.java
@@ -0,0 +1,15 @@
+public class Mobil extends Kendaraan {
+    private int jumlahRoda;
+
+    public Mobil(String merk, String model, int tahunProduksi, int jumlahRoda) {
+        super(merk, model, tahunProduksi);
+        this.jumlahRoda = jumlahRoda;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Jumlah Roda    : " + jumlahRoda);
+        System.out.println("----------------------------");
+    }
+}
