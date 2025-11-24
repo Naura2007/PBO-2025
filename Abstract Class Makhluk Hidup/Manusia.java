9d1becfed1a017581f25a11c847125af802152a5
Naura Rossa Azalia <nauraazalia2007@gmail.com>
2025-11-24 20:41:43 +0700

    Add Manusia class extending MakhlukHidup

---
+++ Abstract Class Makhluk Hidup/Manusia.java
@@ -0,0 +1,12 @@
+public class Manusia extends MakhlukHidup {
+    private String duaKaki;
+
+    public Manusia(String duaKaki) {
+        this.duaKaki = duaKaki;
+    }
+
+    @Override
+    public void berdiri() {
+        System.out.println("Manusia berdiri menggunakan :  " + duaKaki);
+    }
+}
